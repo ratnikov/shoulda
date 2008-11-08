045c9c3f8e925401cd047ce46255807fe784ce13
Joe Ferris <joe.r.ferris@gmail.com>
2008-11-08 15:27:53 -0500

    Added a matcher for checking whether or not an instance accepts a value

--- lib/shoulda/active_record.rb
+++ lib/shoulda/active_record.rb
@@ -1,5 +1,6 @@
 require 'shoulda'
 require 'shoulda/active_record/helpers'
+require 'shoulda/active_record/matchers'
 require 'shoulda/active_record/assertions'
 require 'shoulda/active_record/macros'
 
@@ -7,6 +8,7 @@ module Test # :nodoc: all
   module Unit
     class TestCase
       include ThoughtBot::Shoulda::ActiveRecord::Helpers
+      include ThoughtBot::Shoulda::ActiveRecord::Matchers
       include ThoughtBot::Shoulda::ActiveRecord::Assertions
       extend ThoughtBot::Shoulda::ActiveRecord::Macros
     end
