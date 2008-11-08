bce697a7a462dfbd67544cbdf8b2addc37a5b382
Joe Ferris <joe.r.ferris@gmail.com>
2008-11-08 11:47:20 -0500

    Added assertions to check whether or not a matcher accepts or rejects a given target

--- lib/shoulda/assertions.rb
+++ lib/shoulda/assertions.rb
@@ -44,6 +44,18 @@ module ThoughtBot # :nodoc:
           assert(!collection.include?(x), msg)
         end
       end
+
+      # Asserts that the given matcher returns true when +target+ is passed to #matches?
+      def assert_accepts(matcher, target)
+        success = matcher.matches?(target)
+        assert_block(matcher.failure_message) { success }
+      end
+
+      # Asserts that the given matcher returns false when +target+ is passed to #matches?
+      def assert_rejects(matcher, target)
+        success = !matcher.matches?(target)
+        assert_block(matcher.negative_failure_message) { success }
+      end
     end
   end
 end
